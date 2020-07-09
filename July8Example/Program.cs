972f607bd8489fb33e1fc5b30578bf9972364f98
Rachel McCloud <[Drachelmccloud22@gmail.com>
2020-07-08 22:05:22 -0400

    Initialize Repository

---
+++ July8Example/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace July8Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number");
+            var userInput = Console.ReadLine();
+            Console.WriteLine("enter a number");
+            var seconduserInput = Console.ReadLine();
+            int userInputAsInt = int.Parse(userInput);
+            userInputAsInt++;
+            Console.WriteLine(userInputAsInt);
+            
+
+        }
+    }
+}
